5ee3f91ef3303f8db8e17dfe33018cc03b1bf67c
zarel-blessed <zarelprogramming851815@gmail.com>
2024-11-04 11:27:56 +0530

    Added some react code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
 import { useEffect, useState } from "react"
 
 const App = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<any>([])
+  const [newTodo, setNewTodo] = useState({
+    id: 1,
+    task: "",
+    iscompleted: false
+  })
+
+  async function postTodo(e: any) {
+      e.preventDefault()
+      fetch("http://localhost:3080/todos", {
+        method: "POST",
+        body: JSON.stringify({
+          task: newTodo.task,
+          iscompleted: false
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }).then(resp => resp.json()).then(data => setNewTodo({
+        ...newTodo,
+        id: data.id
+      }))
+
+      setTodos([...todos, newTodo])
+  }
 
   useEffect(() => {
     fetch("http://localhost:3080/todos")
@@ -11,11 +35,18 @@ const App = () => {
 
   return (
     <>
-      <h1 className="text-red-500">Hello, FullStack</h1>
+      <h1 className="text-red-500">Gotodo</h1>
+      <form>
+        <input type="text" onChange={(event) => setNewTodo({
+          ...newTodo,
+          task: event.target.value
+        })} placeholder="Enter a task..." />
+        <input type="submit" onClick={postTodo} />
+      </form>
       <ul>
         {
          todos.map((todo: any) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>{todo.id}</li>
          ))
         }
       </ul>
